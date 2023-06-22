2cdf07cc78cfc35bcba8f9d3e6fe21f341593313
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-06-21 19:46:05 -0500

    Taller 20

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <Contador titulo="Estudiante" :inicio="400+2" />
-  <Contador titulo="Profesor" :inicio="101"/>
+  <CaracolaMagica/>
 </template>
 
 <script>
-import Calculadora from "./components/Calculadora.vue";
-import Contador from "./components/Contador.vue";
+import CaracolaMagica from "./components/CaracolaMagica.vue";
 export default {
   name: "App",
   components: {
-    Contador,
+    CaracolaMagica,
   },
 };
 </script>
